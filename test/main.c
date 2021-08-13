baad14db39a0b84f0387a64ee01ee4a39e529536
Will Pringle <willkantorpringle+1@gmail.com>
2021-08-13 00:35:53 -0400

    Add test c files

---
+++ test/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+  // sum
+  int num = sum(1,2);
+  printf("1 + 2 = %d\n", num);
+
+  // subtract
+  int num2 = subtract(1,2);
+  printf("1 - 2 = %d\n", num2);
+
+  return 0; 
+}
+
